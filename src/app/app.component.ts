85918181a07aef85e41a4fa19719eb5443355ad3
Maksym Shemet <max.shemet@gmail.com>
2022-07-22 17:39:42 +0300

    add post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Test';
+  imgUrl = 'https://picsum.photos/id/237/300/300';
+
+  getName(): string {
+    return this.title;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgUrl = (e.target as HTMLInputElement).value;
+  }
+}
