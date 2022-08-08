03456b62d28ab1b29143f951175e0f6513a7fcc1
Maksym Shemet <max.shemet@gmail.com>
2022-08-08 16:31:53 +0300

    done ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Test';
+  title = 'first last name';
   imgUrl = 'https://picsum.photos/id/237/300/300';
+  images = [
+    'https://picsum.photos/id/237/300/300',
+    'https://picsum.photos/id/237/300/300',
+    'https://picsum.photos/id/237/300/300',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
 
   getName(): string {
     return this.title;
